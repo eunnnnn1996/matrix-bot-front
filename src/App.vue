d2cc9f2952b07eca9a8b05f0f91f448154b4795d
eunnnnn1996 <eunnnnn1996@gmail.com>
2026-02-23 22:34:32 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="min-h-screen bg-black text-green-400 font-mono relative overflow-hidden">
+    <MatrixEffect />
+    
+    <!-- 고정 박스 -->
+    <div>
+      <TradeDashboard />
+      
+      <!-- 예시: 로그 박스 -->
+      <div class="mt-4 flex-1 overflow-auto">
+        <ul>
+          <li v-for="(l, idx) in logs" :key="idx">{{ l }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MatrixEffect from './components/MatrixEffect.vue'
+import TradeDashboard from './components/TradeDashboard.vue'
+export default { components: { MatrixEffect, TradeDashboard } }
+</script>
+
---
+++ src/components/MatrixEffect.vue
@@ -0,0 +1,11 @@
+<template>
+  <canvas ref="canvas" class="fixed inset-0 z-0"></canvas>
+</template>
+
+<script>
+import { onMounted, ref, onBeforeUnmount } from 'vue'
+export default {
+  setup() {
+  }
+}
+</script>
